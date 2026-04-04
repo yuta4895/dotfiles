9747a790ddf8e0c18020405c1798baeb91f96464
Yuta Sakuma <yutasakuma0319@gmail.com>
2026-04-04 22:52:03 +0900

    feat(nvim): add keymaps to open oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,12 @@
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
   ---@type oil.SetupOpts
   opts = {},
-  -- Optional dependencies
+    -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
