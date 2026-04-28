a10ee4c7878bd4e3385e234fed0d168ad388099c
Yuta Sakuma <yutasakuma0319@gmail.com>
2026-04-28 16:35:36 +0900

    feat(nix): install and configure fzf via home-manager

--- nix/home/yuta/home.nix
+++ nix/home/yuta/home.nix
@@ -12,4 +12,46 @@
   programs.bat = {
     enable = true;
   };
-}
+
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      _fzf_compgen_path() {
+        fd --hidden --follow --exclude ".git" . "$1"
+      }
+      _fzf_compgen_dir() {
+        fd --type d --hidden --follow --exclude ".git" . "$1"
+      }
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultCommand = "fd --strip-cwd-prefix --hidden --follow --exclude .git";
+    defaultOptions = [
+      "--style minimal"
+      "--height 40%"
+      "--tmux bottom,40%"
+      "--layout reverse"
+      "--border top"
+    ];
+    fileWidgetCommand = "fd --strip-cwd-prefix --hidden --follow --exclude .git";
+    fileWidgetOptions = [
+      "--walker-skip .git,node_modules,target,venv,build,dist"
+      "--preview 'bat -n --color=always {}'"
+      "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+    ];
+    changeDirWidgetCommand = "fd --type d --strip-cwd-prefix --hidden --follow --exclude .git";
+    changeDirWidgetOptions = [
+      "--walker-skip .git,node_modules,target,venv,build,dist"
+      "--preview 'tree -C {}'"
+      "--bind 'ctrl-/:change-preview-window(down|hidden|)'"
+    ];
+    historyWidgetOptions = [
+      "--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'"
+      "--color header:italic"
+      "--header 'Press CTRL-Y to copy command into clipboard'"
+    ];
+  };
+ }
